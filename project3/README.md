84132331e1f9f4fc90b86e8f9b8dc69dc45c0958
jdbenson <jdbenson@us.ibm.com>
2018-11-08 19:51:35 +0000

    Initial Template Commit

---
+++ project3/README.md
@@ -0,0 +1,8 @@
+## project3 infrastructure
+
+Created 11/8/18 by admin. for project3
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ project3/variables.tf
@@ -0,0 +1,56 @@
+#####################################################################
+##
+##      Created 11/8/18 by admin. for project3
+##
+#####################################################################
+
+variable "openstack_user_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_password" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_project_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_domain_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_auth_url" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_region" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "RHEL73-Proj3_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_image_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_flavor_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "openstack_key_pair_name" {
+  type = "string"
+  description = "Generated"
+}
+
